55284c8e12560bad2fcac074a86e9d0d657567d1
Sergey <pettsonchello@gmail.com>
2014-07-06 02:34:44 +0400

    Промежуточный коммит

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
-    
+    $('.b-product-mini__image').click(function(e) {
+
+        $.fancybox($(this).parents('.b-catalog').find('.b-product-full'));
+
+        e.preventDefault();
+    });
 
 });
